0f339f4709b8db998bf44e834787d0a6a8791035
rakesh <rak1994kumar@gmail.com>
2016-10-18 13:06:21 -0700

    upload.js files has been changed

--- Utils/convertFileSystem.js
+++ Utils/convertFileSystem.js
@@ -1,10 +1,17 @@
-var convertBytesToKb = function (bytes) {
+var convertBytesToKb = function (bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    var units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
 
-    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes == 0) return '0 Byte';
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1) + ' ' + units[u];
 
 }
 
--- routes/upload.js
+++ routes/upload.js
@@ -11,17 +11,17 @@ var sizeCal = require('../Utils/convertFileSystem.js');
 var isDebug = false;
 var DEBUG = function (val) {
     if (isDebug) {
-        console.log("SpringDataRestApi : LOG : ");
+        console.log("NodeRestApi : LOG : ");
         console.log(val);
     }
 };
 var ERROR = function (val) {
-    console.log("SpringDataRestApi : ERROR : ");
+    console.log("NodeRestApi : ERROR : ");
     console.log(val);
 };
 
 var LOG = function (val) {
-    console.log("SpringDataRestApi : LOG : ");
+    console.log("NodeRestApi : LOG : ");
     console.log(val);
 };
 
@@ -32,7 +32,28 @@ var UploadObject = {
         var name = req.file.filename;
         var originalFileName = req.file.originalname;
         var sizeOfFile = req.file.size;
-        var ExactSize =  sizeCal.convertBytesToKb(sizeOfFile);
+        var ExactSize = sizeCal.convertBytesToKb(sizeOfFile, true);
+        var dirName = 'uploadCSV';
+
+        if (!fs.existsSync(dirName)) {
+            fs.mkdirSync(dirName);
+        }
+
+        try {
+            DEBUG(response);
+        } catch (exception) {
+            console.log("Exception :" + exception);
+        }
+
+
+
+
+
+        console.log("" + ExactSize);
+        return res.json({
+            messages: "The size of the Fies is",
+            fileSize: "" + ExactSize
+        })
 
 
     }
