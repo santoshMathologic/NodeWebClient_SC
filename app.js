46e824b0533af210a4b72dcdac39fc168207af53
rakesh <rak1994kumar@gmail.com>
2016-10-17 17:27:36 -0700

    fav icon added

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.engine('html', cons.swig);
 // set .html as the default extension
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+//app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
